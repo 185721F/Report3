ecb556207cc2fb3ae32d8f0f5fe185b570150a4f
Kaito Arakaki <e185721@ie.u-ryukyu.ac.jp>
2018-11-12 19:51:59 +0900

    Unittest firstcode

--- src/main/jp/ac/uryukyu/ie/e185721/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e185721/Enemy.java
@@ -39,8 +39,12 @@ public class Enemy {
      */
     public void attack(Hero hero){
         int damage = (int)(Math.random() * attack);
-        System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-        hero.wounded(damage);
+        if(dead == false){
+            hero.wounded(damage);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+        }
+
+
     }
 
     /**
